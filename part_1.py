e606dc5a17776a51615a4814fef3f5bf229bb6f0
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-11 11:32:55 +0000

    change 3

--- part_1.py
+++ part_1.py
@@ -1,8 +1,17 @@
 sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+new_findings=[]
 print(sample_bay[0])
 x = len(sample_bay) 
 print(sample_bay[x - 1])
 print(x)
 for salts in sample_bay:
     print(f"transmitting data for:", salts)
+while len(new_findings)<3:
+    y=input("type of salt?")
+    new_findings.append(y)
+print(new_findings)
+sample_bay.remove("Dust")
+print(sample_bay)
+  
 
+    
