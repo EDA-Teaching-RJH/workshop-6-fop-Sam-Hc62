9e8d39a9c22d1ad07d21d3b50dee7bc422c9e3d6
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-11 12:55:17 +0000

    change 8

--- part_4.py
+++ part_4.py
@@ -1,1 +1,21 @@
+travel_log = []
+while True:
+    try:
+        X = int(input("Sensor Reading (Slope Angle):"))
+        if X >45:
+            print("CRITICAL TILT! HALTING.")
+            print("mission terminated")
+            break
+        else:
+            travel_log.append(X)
+            print("Path Stable. Moving forward.")
+            print(travel_log)
+            continue
+    except ValueError:
+        print("sensor glitch")
+    else:
+        break
+print(travel_log)
+
+
 
