77439c35a26fed6a26b3698197c3964dce5d95ee
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-11 12:33:12 +0000

    change 6

--- part_3.py
+++ part_3.py
@@ -1,1 +1,21 @@
+try:
+    speed = int(input("Enter motor speed"))
+    if 0<=speed<=100:
+        print("speed set to:", speed)
+    else:
+        print("Invalid speed")
+except ValueError:
+    print("Error: Corrupted Signal. Maintaining current speed.")
+
+def get_coordinate():
+    while True:
+        try:
+            X = int(input("x coordinate"))
+        except ValueError:
+            print("invalid coordinate")
+        else:
+            break
+get_coordinate()
+
+
 
