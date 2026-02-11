59e4b62962a80c1e7d03ea22a046bb4a70627b6f
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-11 12:05:50 +0000

    change 5

--- part_2.py
+++ part_2.py
@@ -7,3 +7,10 @@ x = dict(list(rover_status.items())[:1])
 print(x)
 rover_status.update({"battery":85,"heater":"on","speed":5})
 print(rover_status)
+Dictionary_1 = {"Site": "Crater A", "Radiation": "Low", "Water": False}
+Dictionary_2 = {"Site": "Dune B", "Radiation": "High", "Water": True}
+mission_log = [Dictionary_1,Dictionary_2]
+for values in mission_log:
+    site = values["Site"]
+    radiation = values["Radiation"]
+    print(f"{site} has {radiation} radiation")
