28320245212b361d950ee7f22e1f9cafc48fbee5
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-11 11:50:51 +0000

    change 4

--- part_2.py
+++ part_2.py
@@ -1,1 +1,9 @@
-
+rover_status = {
+    "battery":100,
+    "heater":"off",
+    "camera":"standby"
+}
+x = dict(list(rover_status.items())[:1])
+print(x)
+rover_status.update({"battery":85,"heater":"on","speed":5})
+print(rover_status)
