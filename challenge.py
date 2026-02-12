39281de451cb0da8f5c3557eefc2c759d9557c00
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-12 10:39:56 +0000

    change 10

--- challenge.py
+++ challenge.py
@@ -1,1 +1,29 @@
+command_batch = [
+    "MOVE 10",
+    "TURN LEFT",
+    "MOVE 5",
+    "MOVE five",    # Corrupted: 'five' is text, not a number
+    "DIG",
+    "MOVE 20",
+    "XÆA-12",       # Corrupted: Unknown command
+    "RETURN",
+    "MOVE 15"
+]
+rover_state = {"x": 0, "y": 0, "samples": 0}
+for items in command_batch:
+    split_command = items.split()
+    if split_command[0] == "MOVE":
+        if split_command[1].isdigit():
+            rover_state["y"] + int(split_command[1])
+            print(rover_state)
+        else:
+            print("value error")
+    elif split_command[0] == "DIG":
+        rover_state["samples"] + 1
+    elif split_command[0] == "TURN":
+        print("Turning...")
+    else:
+        print("Error: Unknown command sequence")
+print(rover_state)
+        
 
